6a009684c46e542dc1c38a34f96b64ed4e1de0f2
GliczDev <67753196+GliczDev@users.noreply.github.com>
2024-10-19 17:19:13 +0200

    Initial client-mache commit

--- versions/1.21.10/build.gradle.kts
+++ versions/1.21.10/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 val generateReportsProperty = providers.gradleProperty("generateReports")
 mache {
     minecraftVersion = "1.21.10"
-    minecraftJarType = MinecraftJarType.SERVER
+    minecraftJarType = MinecraftJarType.CLIENT
 
     val args = mutableListOf(
         "--temp-dir={tempDir}",
